6b4abbda04dfb1b2ff83c221645592e68c88965b
evecus <jmfu003@gmail.com>
2025-11-24 22:03:33 +0800

    Change allowed TCP port from 6666 to 8899

--- sh/set-ufw-rules.sh
+++ sh/set-ufw-rules.sh
@@ -3,7 +3,7 @@ ufw default deny incoming
 ufw default allow outgoing
 ufw allow from 192.168.0.0/16
 ufw allow from 172.16.0.0/12
-ufw allow 6666/tcp
+ufw allow 8899/tcp
 ufw enable
 ufw status verbose
 systemctl enable ufw
