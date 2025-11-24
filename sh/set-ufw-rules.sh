169203aa1a7b2f74e56191dd21adc5b14e688bfb
evecus <jmfu003@gmail.com>
2025-11-24 22:00:59 +0800

    Update UFW rules to allow 172.16.0.0/12

--- sh/set-ufw-rules.sh
+++ sh/set-ufw-rules.sh
@@ -4,7 +4,7 @@ ufw default allow outgoing
 ufw allow from 192.168.1.0/24
 ufw allow from 192.168.0.0/24
 ufw allow from 192.168.31.0/24
-ufw allow from 172.31.0.0/24
+ufw allow from 172.16.0.0/12
 ufw allow 6666/tcp
 ufw enable
 ufw status verbose
