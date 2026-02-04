cfd03b8a410628c47e2f92905c256422daf66f7d
evecus <jamie55@163.com>
2026-02-04 21:36:23 +0800

    Update set-ufw-rules.sh

--- sh/set-ufw-rules.sh
+++ sh/set-ufw-rules.sh
@@ -1,6 +1,7 @@
 apt install ufw
 ufw default deny incoming
 ufw default allow outgoing
+ufw default allow routed
 ufw allow from 192.168.0.0/16
 ufw allow from 172.16.0.0/12
 ufw allow from 10.0.0.0/24
